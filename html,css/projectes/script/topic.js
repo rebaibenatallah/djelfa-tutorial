23d7c954232dd68844970e959dd0c24bfb105c66
benatallah <benatallahrebai@gmail.com>
2025-05-30 23:23:32 +0100

    Topic third content box and fifth one and timeline , tabs tools

--- html,css/projectes/script/topic.js
+++ html,css/projectes/script/topic.js
@@ -8,3 +8,23 @@ function scrollFunction() {
     header.classList.remove('active');
   }
 }
+
+// third content box
+const btns =document.querySelectorAll('.btns-category .btn');
+const pages = document.querySelectorAll('.pages> *');
+btns.forEach((btn,index)=>{
+  btn.addEventListener('click',(e)=>{
+    btns.forEach((btn)=>{
+      btn.classList.remove('active');
+    });
+    btn.classList.add('active');
+    var line = document.querySelector('.line');
+    line.style.width = e.target.offsetWidth + "px";
+    console.log(line);
+    line.style.left = e.target.offsetLeft + "px";
+    pages.forEach((page)=>{
+      page.classList.remove('active');
+    });
+    pages[index].classList.add('active');
+  });
+});
